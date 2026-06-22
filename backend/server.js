23e2d78ff4234f51a9e2bae2851a7df684731976
MayerTagadkar2309 <mayurtagadkar9@gmail.com>
2026-06-22 16:23:35 +0530

    updeted code for package.jsx & server.js

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,8 @@ app.post("/translate", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server Running on Port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server Running on Port ${PORT}`);
 });
